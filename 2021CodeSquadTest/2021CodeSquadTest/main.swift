320951879a377ab0a6ea74ca7a2337c1e0822b51
dahun-lee-daji <ssss6596@gmail.com>
2020-12-13 07:31:42 +0900

    큐브 이동 기능 구현 중
    명령 처리 함수 구현
    큐브 사용시간 메서드화
    이동 횟수 카운팅 관련 메서드 구현
    2D상에서 CW,CCW회전 구현
    3D상 큐브 회전 구현 중

--- 2021CodeSquadTest/2021CodeSquadTest/main.swift
+++ 2021CodeSquadTest/2021CodeSquadTest/main.swift
@@ -24,18 +24,77 @@ struct Cube2D {
         bottomLayer = []
     }
     
-    func getColumn(_ columnOrder : Int)-> String {
-        return topLayer[columnOrder] + " " + middleLayer[columnOrder] + " " + bottomLayer[columnOrder]
+    mutating func turnRight() {
+        let tempside = self
+        self.topLayer = tempside.getIndicedColumn(columnOrder: 0, direction: false)
+        self.middleLayer = tempside.getIndicedColumn(columnOrder: 1, direction: false)
+        self.bottomLayer = tempside.getIndicedColumn(columnOrder: 2, direction: false)
+    }
+    mutating func turnLeft() {
+        let tempside = self
+        self.topLayer = tempside.getIndicedColumn(columnOrder: 2, direction: true)
+        self.middleLayer = tempside.getIndicedColumn(columnOrder: 1, direction: true)
+        self.bottomLayer = tempside.getIndicedColumn(columnOrder: 0, direction: true)
     }
     
-    func getRow(_ rowOrder : Int)-> String {
+    func doIndiceString(_ input : String) -> [String] {
+        var item = [String]()
+        for i in input.indices {
+            item.append(String(input[i]))
+        }
+        return item
+    }
+    
+    func getIndicedColumn(columnOrder : Int, direction : Bool)-> [String] {
+        var item = String()
+        if direction {
+            item = topLayer[columnOrder] + middleLayer[columnOrder] + bottomLayer[columnOrder]
+        } else {
+            item = bottomLayer[columnOrder] + middleLayer[columnOrder] + topLayer[columnOrder]
+        }
+        return doIndiceString(item)
+    }
+    mutating func setRow(input:ArraySlice<String> ,rowOrder : Int) {
+        var item = input
         switch rowOrder {
         case 0:
-            return topLayer[0] + " " + topLayer[1] + " " + topLayer[2]
+            for i in 0..<input.count {
+                topLayer[i] = item.popFirst()!
+            }
         case 1:
-            return middleLayer[0] + " " + middleLayer[1] + " " + middleLayer[2]
+            for i in 0..<input.count {
+                middleLayer[i] = item.popFirst()!
+            }
         case 2:
+            for i in 0..<input.count {
+                bottomLayer[i] = item.popFirst()!
+            }
+        default:
+            ""
+        }
+    }
+    
+    mutating func setColumn(input: ArraySlice<String>,columnOrder : Int) {
+        var item = input
+        topLayer[columnOrder] = item.popFirst()!
+        middleLayer[columnOrder] = item.popFirst()!
+        bottomLayer[columnOrder] = item.popFirst()!
+    }
+    
+    func getRow(rowOrder : Int, direction : Bool)-> String {
+        switch (rowOrder, direction) {
+        case (0,true):
+            return topLayer[0] + " " + topLayer[1] + " " + topLayer[2]
+        case (1,true):
+            return middleLayer[0] + " " + middleLayer[1] + " " + middleLayer[2]
+        case (2,true):
             return bottomLayer[0] + " " + bottomLayer[1] + " " + bottomLayer[2]
+        case (0,false):
+            return topLayer[2] + " " + topLayer[1] + " " + topLayer[0]
+        case (1,false):
+            return middleLayer[2] + " " + middleLayer[1] + " " + middleLayer[0]
+        case (2,false):
+            return bottomLayer[2] + " " + bottomLayer[1] + " " + bottomLayer[0]
         default:
             return ""
         }
@@ -43,16 +102,13 @@ struct Cube2D {
     
     func print2DCube(frontSpacing : Int = 0) {
         let spacing = String(repeating: " ", count: frontSpacing)
-            print("\(spacing)\(getRow(0)) ")
-            print("\(spacing)\(getRow(1)) ")
-        print("\(spacing)\(getRow(2)) ", terminator : "\n\n")
+        print("\(spacing)\(getRow(rowOrder:0, direction: true)) ")
+        print("\(spacing)\(getRow(rowOrder:1, direction: true)) ")
+        print("\(spacing)\(getRow(rowOrder:2, direction: true )) ", terminator : "\n\n")
         }
 }
 
 struct Cube3D {
-    var cubeSides = [Cube2D](repeating: Cube2D(), count: 6)
-    // cubeSides[0] : 윗면, 1234 좌 앞 우 뒤, 5 아래면
-    
     enum NumberingSides : Int {
         case upper = 0
         case left = 1
@@ -61,18 +117,38 @@ struct Cube3D {
         case back = 4
         case bottom = 5
         
+        func upper() -> Int {
+            return NumberingSides.upper.rawValue
+        }
+        func left() -> Int {
+            return NumberingSides.left.rawValue
+        }
+        func front() -> Int {
+            return NumberingSides.front.rawValue
+        }
+        func right() -> Int {
+            return NumberingSides.right.rawValue
+        }
+        func back() -> Int {
+            return NumberingSides.back.rawValue
+        }
+        func bottom() -> Int {
+            return NumberingSides.bottom.rawValue
+        }
     }
+    var cubeSides = [Cube2D](repeating: Cube2D(), count: 6)
     
+    let startTime = Date.init(timeIntervalSinceNow: 0)
     var side : NumberingSides = .front
     var moves = Int()
     
     init (){
-        cubeSides[Cube3D.NumberingSides.upper.rawValue] = .init(color: "B")
-        cubeSides[Cube3D.NumberingSides.left.rawValue] = .init(color: "W")
-        cubeSides[Cube3D.NumberingSides.front.rawValue] = .init(color: "O")
-        cubeSides[Cube3D.NumberingSides.right.rawValue] = .init(color: "G")
-        cubeSides[Cube3D.NumberingSides.back.rawValue] = .init(color: "Y")
-        cubeSides[Cube3D.NumberingSides.bottom.rawValue] = .init(color: "R")
+        cubeSides[side.upper()] = .init(color: "B")
+        cubeSides[side.left()] = .init(color: "W")
+        cubeSides[side.front()] = .init(color: "O")
+        cubeSides[side.right()] = .init(color: "G")
+        cubeSides[side.back()] = .init(color: "Y")
+        cubeSides[side.bottom()] = .init(color: "R")
     }
     
     func print3DCube(){
@@ -82,16 +158,137 @@ struct Cube3D {
         
         cubeSides[Cube3D.NumberingSides.upper.rawValue].print2DCube(frontSpacing: 15)
         for i in 1...4 {
-            middleSidesTopLayer += "     \(cubeSides[i].getRow(0))"
-            middleSidesMiddleLayer += "     \(cubeSides[i].getRow(1))"
-            middleSidesBottomLayer += "     \(cubeSides[i].getRow(2))"
+            middleSidesTopLayer += "     \(cubeSides[i].getRow(rowOrder: 0, direction: true))"
+            middleSidesMiddleLayer += "     \(cubeSides[i].getRow(rowOrder: 1, direction: true))"
+            middleSidesBottomLayer += "     \(cubeSides[i].getRow(rowOrder: 2, direction: true))"
         }
         print(middleSidesTopLayer, middleSidesMiddleLayer, middleSidesBottomLayer, separator: "\n",terminator:"\n\n")
         cubeSides[Cube3D.NumberingSides.bottom.rawValue].print2DCube(frontSpacing: 15)
     }
+    
+    mutating func turnRight(target : NumberingSides) {
+        moves += 1
+        switch target {
+        case .front:
+            setRightMovedRow(target: target)
+            cubeSides[target.rawValue].turnRight()
+        case .back:
+            setLeftMovedRow(target: target)
+            cubeSides[target.rawValue].turnLeft()
+        case .upper:
+            setRightMovedRow(target: target)
+            cubeSides[target.rawValue].turnRight()
+        case .bottom:
+            setLeftMovedRow(target: target)
+            cubeSides[target.rawValue].turnLeft()
+        case .left:
+            setLeftMovedRow(target: target)
+            cubeSides[target.rawValue].turnLeft()
+        case .right:
+            setRightMovedRow(target: target)
+            cubeSides[target.rawValue].turnRight()
+        
+        default:
+            print("Error: 잘못된 입력 in turnRight()", self)
+        }
+    }
+    
+    mutating func setRightMovedRow(target : NumberingSides) {
+        var movingRow = String()
+        var indicedMovedRow = [String]()
+        switch target {
+        case .front:
+            movingRow = cubeSides[side.left()].getIndicedColumn(columnOrder: 2, direction: false).joined() + cubeSides[side.upper()].getRow(rowOrder: 2,direction: true).components(separatedBy: " ").joined() + cubeSides[side.right()].getIndicedColumn(columnOrder: 0, direction: true).joined() + cubeSides[side.bottom()].getRow(rowOrder: 0, direction: false).components(separatedBy: " ").joined()
+            indicedMovedRow = doIndiceString(movingCellsRight(movingRow, 3))
+            cubeSides[side.left()].setColumn(input: indicedMovedRow[0...2], columnOrder: 2)
+            cubeSides[side.upper()].setRow(input: indicedMovedRow[3...5], rowOrder: 2)
+            cubeSides[side.right()].setColumn(input: indicedMovedRow[6...8], columnOrder: 0)
+            cubeSides[side.bottom()].setRow(input: indicedMovedRow[9...11], rowOrder: 0)
+        case .back:
+            movingRow = cubeSides[side.left()].getIndicedColumn(columnOrder: 0, direction: false).joined() + cubeSides[side.upper()].getRow(rowOrder: 0,direction: true).components(separatedBy: " ").joined() + cubeSides[side.right()].getIndicedColumn(columnOrder: 2, direction: true).joined() + cubeSides[side.bottom()].getRow(rowOrder: 2, direction: false).components(separatedBy: " ").joined()
+            indicedMovedRow = doIndiceString(movingCellsRight(movingRow, 3))
+            cubeSides[side.left()].setColumn(input: indicedMovedRow[0...2], columnOrder: 0)
+            cubeSides[side.upper()].setRow(input: indicedMovedRow[3...5], rowOrder: 0)
+            cubeSides[side.right()].setColumn(input: indicedMovedRow[6...8], columnOrder: 2)
+            cubeSides[side.bottom()].setRow(input: indicedMovedRow[9...11], rowOrder: 2)
+        default:
+            ""
+        }
+    }
+    
+    mutating func setLeftMovedRow(target : NumberingSides) {
+        var movingRow = String()
+        var indicedMovedRow = [String]()
+        switch target {
+        case .front:
+            movingRow = cubeSides[side.left()].getIndicedColumn(columnOrder: 2, direction: false).joined() + cubeSides[side.upper()].getRow(rowOrder: 2,direction: true).components(separatedBy: " ").joined() + cubeSides[side.right()].getIndicedColumn(columnOrder: 0, direction: true).joined() + cubeSides[side.bottom()].getRow(rowOrder: 0, direction: false).components(separatedBy: " ").joined()
+            indicedMovedRow = doIndiceString(movingCellsLeft(movingRow, 3))
+            cubeSides[side.left()].setColumn(input: indicedMovedRow[0...2], columnOrder: 2)
+            cubeSides[side.upper()].setRow(input: indicedMovedRow[3...5], rowOrder: 2)
+            cubeSides[side.right()].setColumn(input: indicedMovedRow[6...8], columnOrder: 0)
+            cubeSides[side.bottom()].setRow(input: indicedMovedRow[9...11], rowOrder: 0)
+        case .back:
+            movingRow = cubeSides[side.left()].getIndicedColumn(columnOrder: 0, direction: false).joined() + cubeSides[side.upper()].getRow(rowOrder: 0,direction: true).components(separatedBy: " ").joined() + cubeSides[side.right()].getIndicedColumn(columnOrder: 2, direction: true).joined() + cubeSides[side.bottom()].getRow(rowOrder: 2, direction: false).components(separatedBy: " ").joined()
+            indicedMovedRow = doIndiceString(movingCellsLeft(movingRow, 3))
+            cubeSides[side.left()].setColumn(input: indicedMovedRow[0...2], columnOrder: 0)
+            cubeSides[side.upper()].setRow(input: indicedMovedRow[3...5], rowOrder: 0)
+            cubeSides[side.right()].setColumn(input: indicedMovedRow[6...8], columnOrder: 2)
+            cubeSides[side.bottom()].setRow(input: indicedMovedRow[9...11], rowOrder: 2)
+        default:
+            ""
+        }
+    }
+    
+    func movingCellsRight(_ inputSentence : String, _ movingCount : Int) -> String {
+            var item = String()
+            let boundaryIndex = inputSentence.index(inputSentence.endIndex, offsetBy: -movingCount)
+            item = inputSentence.substring(from:  boundaryIndex) + inputSentence.substring(to: boundaryIndex)
+            return item
+        }
+
+    func movingCellsLeft(_ inputSentence : String, _ movingCount : Int) -> String {
+        var item = String()
+        let boundaryIndex = inputSentence.index(inputSentence.startIndex, offsetBy: movingCount)
+        item = inputSentence.substring(from:  boundaryIndex) + inputSentence.substring(to: boundaryIndex)
+        return item
+    }
+    
+    func exitCubing() -> Bool {
+        printExitTime()
+        printNumberOfPerformances()
+        print("이용해주셔서 감사합니다. 뚜뚜뚜.")
+        return true
+    }
+    
+    func printNumberOfPerformances() {
+        print("조작갯수: \(moves)")
+    }
+    
+    func printExitTime() {
+        
+        let end = Date.init(timeIntervalSinceNow: 0)
+        print("경과시간: ",calculateIntervalTime(startTime,end) ?? "")
+    }
+    
+    func calculateIntervalTime(_ start : Date, _ end : Date) -> String? {
+        let totalSec = end.timeIntervalSince(start)
+        let dateFormat = DateComponentsFormatter()
+        dateFormat.unitsStyle = .positional
+        dateFormat.allowedUnits = [.minute, .second]
+        dateFormat.zeroFormattingBehavior = .pad
+        
+        return dateFormat.string(from: totalSec)
+    }
+    
+    func doIndiceString(_ input : String) -> [String] {
+        var item = [String]()
+        for i in input.indices {
+            item.append(String(input[i]))
+        }
+        return item
+    }
 }
 
-let start = Date.init(timeIntervalSinceNow: 0)
 
 var currentCube = Cube3D.init()
 currentCube.print3DCube()
@@ -102,22 +299,12 @@ while(true){
     let inputStringMessage = doOptionalBinding(inputOptionalMessage)
     var dicedMessage = doIndiceString(inputStringMessage)
     dicedMessage = insertAdditionalCommands(dicedMessage)
-    print(dicedMessage)
-
+    if doCommands(dicedMessage,&currentCube) {
+            break
+        }
 }
-let end = Date.init(timeIntervalSinceNow: 0)
-print("경과시간: ",calculateIntervalTime(start,end) ?? "")
 
 
-func calculateIntervalTime(_ start : Date, _ end : Date) -> String? {
-    let totalSec = end.timeIntervalSince(start)
-    let dateFormat = DateComponentsFormatter()
-    dateFormat.unitsStyle = .positional
-    dateFormat.allowedUnits = [.minute, .second]
-    dateFormat.zeroFormattingBehavior = .pad
-    
-    return dateFormat.string(from: totalSec)
-}
 
 
 func doOptionalBinding(_ input : Optional<String>) -> String {
@@ -157,4 +344,55 @@ func insertAdditionalCommands(_ input : [String]) -> [String] {
         }
     }
     return item
+}
+
+func doCommands(_ commands : [String], _ cube : inout Cube3D) -> Bool {
+    var exitFlag = false
+    for i in 0..<commands.count {
+        switch commands[i] {
+        case "F":
+            print(commands[i])
+            cube.turnRight(target: Cube3D.NumberingSides.front)
+            cube.print3DCube()
+        case "F'":
+            print(commands[i])
+            cube.print3DCube()
+        case "B":
+            print(commands[i])
+            cube.turnRight(target: Cube3D.NumberingSides.back)
+            cube.print3DCube()
+        case "B'":
+            print(commands[i])
+            cube.print3DCube()
+        case "R":
+            print(commands[i])
+            cube.print3DCube()
+        case "R'":
+            print(commands[i])
+            cube.print3DCube()
+        case "L":
+            print(commands[i])
+            cube.print3DCube()
+        case "L'":
+            print(commands[i])
+            cube.print3DCube()
+        case "U":
+            print(commands[i])
+            cube.print3DCube()
+        case "U'":
+            print(commands[i])
+            cube.print3DCube()
+        case "D":
+            print(commands[i])
+            cube.print3DCube()
+        case "D'":
+            print(commands[i])
+            cube.print3DCube()
+        case "Q":
+            exitFlag = cube.exitCubing()
+        default:
+            print("잘못된 명령어 입니다, 입력된 명령 : \(commands[i])")
+        }
+    }
+    return exitFlag
 }
